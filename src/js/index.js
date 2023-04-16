f1d69420403dfb8fc82eea8883ca9341981770a6
vampirerachel <vampirerachel@gmail.com>
2023-04-16 17:22:30 -0400

    working

--- src/js/index.js
+++ src/js/index.js
@@ -1,58 +1,56 @@
 import '../css/styles.css';
+import { createMarkup, onShowCountryList } from '../markeup';
 import fetchCountries from './fetchCountries'
+import { debounce } from 'lodash';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector("#search-box")
 const countryList = document.querySelector(".country-list")
 const countryInfoEl = document.querySelector(".country-info")
 
+inputEl.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY) ); 
 
-const handleInput = function checkApi(e) {
+function handleInput(e) {
 
     const userInput = e.target.value.trim();
-        let data = fetchCountries(userInput)
 
-    if (userInput === '') { countryList.innerHTML = ''; }
-    
-        if(userInput !== '') {
-        
-            data.then(res => {
-        
-        fetchCountries(value)
-
-        function onShowCountryList({ flags, name }) {return `
-        <li style="display:flex;">
-        <img style="margin-right: 15px;" src="${res[0].flags.svg}" alt="${res[0].name.official}" width=50/>
-        <h2>${res[0].name.official}</h2>
-        </li>
-        `;}
-
-
-
-        function createMarkup ({ flags, name, capital, population, languages}){return `
-        <div style='display: flex; align-items: center;'>
-            <img style="margin-right: 15px;" src='${flags.svg}' alt='${name.official}' width=100 />
-            <h2 style="font-size: 36px; font-weight: 700;">${name.official}</h2>
-        </div>
-        <div style='display: flex; align-items: center;'>
-            <p style="margin-right: 15px; font-size: 28px; font-weight: 500; color: darkgreen; ">Capital:</p>
-            <span style="font-size: 30px; font-weight: 700; color: darkblue; ">${capital}</span>
-        </div>
-        <div style='display: flex; align-items: center;'>
-            <p style="margin-right: 15px; font-size: 28px; font-weight: 500; color: darkgreen; ">Population:</p>
-            <span style="font-size: 30px; font-weight: 700; color: darkblue; ">${population}</span>
-        </div>
-        <div style='display: flex; align-items: center;'>
-            <p style="margin-right: 15px; font-size: 28px; font-weight: 500; color: darkgreen; ">Languages:</p>
-            <span style="font-size: 30px; font-weight: 700; color: darkblue; ">${Object.values(languages)}</span>
-        </div>
-        `};
-
-
-        })};
+    if (userInput === '') {
+        countryList.innerHTML = '';
+        countryInfoEl.innerHTML = '';
     }
-//TODO we need to debounce our addEventListener for every 300ms
+    
+    if (userInput) {
+        fetchCountries(userInput).then(onSuccess).catch(onError)
+    };
+    
+    function onSuccess(data) {
 
-inputEl.addEventListener('input', handleInput); 
+        if (data.length > 10) {
+            Notify.info("Too many Countries")
+            countryInfoEl.innerHTML = '';
+            countryList.innerHTML = '';
+        }
+        
+        if (data.length >= 2 && data.length <= 10) {
+            countryList.innerHTML = data.map
+                (country => onShowCountryList(country)).join('');
+        }
+
+        if (data.length === 1) {
+            countryList.innerHTML = '';
+            countryInfoEl.innerHTML = data.map
+                (country => createMarkup(country)).join('')
+        }
+    }
+    function onError(error) {
+        Notify.info('Oops, there is no country with that name');
+        countryInfoEl.innerHTML = '';
+        countryList.innerHTML = '';
+        throw new Error(error);
+    }
+}
+
+
 
 
     
---
+++ src/markeup.js
@@ -0,0 +1,25 @@
+    export function createMarkup ({ flags, name, capital, population, languages}){return `
+        <div style='display: flex; align-items: center;'>
+            <img style="margin-right: 15px;" src='${flags.svg}' alt='${name.official}' width=100 />
+            <h2 style="font-size: 36px; font-weight: 700;">${name.official}</h2>
+        </div>
+        <div style='display: flex; align-items: center;'>
+            <p style="margin-right: 15px; font-size: 28px; font-weight: 500; color: darkgreen; ">Capital:</p>
+            <span style="font-size: 30px; font-weight: 700; color: darkblue; ">${capital}</span>
+        </div>
+        <div style='display: flex; align-items: center;'>
+            <p style="margin-right: 15px; font-size: 28px; font-weight: 500; color: darkgreen; ">Population:</p>
+            <span style="font-size: 30px; font-weight: 700; color: darkblue; ">${population}</span>
+        </div>
+        <div style='display: flex; align-items: center;'>
+            <p style="margin-right: 15px; font-size: 28px; font-weight: 500; color: darkgreen; ">Languages:</p>
+            <span style="font-size: 30px; font-weight: 700; color: darkblue; ">${Object.values(languages)}</span>
+        </div>
+        `};
+
+export         function onShowCountryList({ flags, name }) {return `
+        <li style="display:flex;">
+        <img style="margin-right: 15px;" src="${flags.svg}" alt="${name.official}" width=50/>
+        <h2>${name.official}</h2>
+        </li>
+        `;}
